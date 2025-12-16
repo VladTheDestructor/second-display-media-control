3411422864034b4e2291629918028671a5119e46
VladTheDestructor <vladkaravaev1@gmail.com>
2025-12-16 23:36:59 +0300

    Add main window layout and basic meida import

--- second-display-media-control/Form1.Designer.cs
+++ second-display-media-control/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace second_display_media_control
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,226 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            PreviewBox = new PictureBox();
+            toolStrip1 = new ToolStrip();
+            toolStripButton1 = new ToolStripButton();
+            toolStripButton2 = new ToolStripButton();
+            toolStripButton3 = new ToolStripButton();
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            newProjectToolStripMenuItem = new ToolStripMenuItem();
+            openProjectToolStripMenuItem = new ToolStripMenuItem();
+            saveProjectToolStripMenuItem = new ToolStripMenuItem();
+            importMediaToolStripMenuItem = new ToolStripMenuItem();
+            importFolderToolStripMenuItem = new ToolStripMenuItem();
+            viewToolStripMenuItem = new ToolStripMenuItem();
+            secondDisplayWidowtempToolStripMenuItem = new ToolStripMenuItem();
+            AddMediaDialog = new OpenFileDialog();
+            listView1 = new ListView();
+            imageList1 = new ImageList(components);
+            FileFromListContextMenu = new ContextMenuStrip(components);
+            moveUpToolStripMenuItem = new ToolStripMenuItem();
+            moveDownToolStripMenuItem = new ToolStripMenuItem();
+            removeFromPlaylistToolStripMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)PreviewBox).BeginInit();
+            toolStrip1.SuspendLayout();
+            menuStrip1.SuspendLayout();
+            FileFromListContextMenu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // PreviewBox
+            // 
+            PreviewBox.Location = new Point(630, 67);
+            PreviewBox.Name = "PreviewBox";
+            PreviewBox.Size = new Size(382, 230);
+            PreviewBox.TabIndex = 0;
+            PreviewBox.TabStop = false;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Dock = DockStyle.None;
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton1, toolStripButton2, toolStripButton3 });
+            toolStrip1.Location = new Point(769, 312);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(81, 25);
+            toolStrip1.TabIndex = 2;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton1.Image = (Image)resources.GetObject("toolStripButton1.Image");
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(23, 22);
+            toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripButton2
+            // 
+            toolStripButton2.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton2.Image = (Image)resources.GetObject("toolStripButton2.Image");
+            toolStripButton2.ImageTransparentColor = Color.Magenta;
+            toolStripButton2.Name = "toolStripButton2";
+            toolStripButton2.Size = new Size(23, 22);
+            toolStripButton2.Text = "toolStripButton2";
+            // 
+            // toolStripButton3
+            // 
+            toolStripButton3.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton3.Image = (Image)resources.GetObject("toolStripButton3.Image");
+            toolStripButton3.ImageTransparentColor = Color.Magenta;
+            toolStripButton3.Name = "toolStripButton3";
+            toolStripButton3.Size = new Size(23, 22);
+            toolStripButton3.Text = "toolStripButton3";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, viewToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1119, 24);
+            menuStrip1.TabIndex = 3;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { newProjectToolStripMenuItem, openProjectToolStripMenuItem, saveProjectToolStripMenuItem, importMediaToolStripMenuItem, importFolderToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // newProjectToolStripMenuItem
+            // 
+            newProjectToolStripMenuItem.Name = "newProjectToolStripMenuItem";
+            newProjectToolStripMenuItem.Size = new Size(146, 22);
+            newProjectToolStripMenuItem.Text = "New project";
+            // 
+            // openProjectToolStripMenuItem
+            // 
+            openProjectToolStripMenuItem.Name = "openProjectToolStripMenuItem";
+            openProjectToolStripMenuItem.Size = new Size(146, 22);
+            openProjectToolStripMenuItem.Text = "Open project";
+            // 
+            // saveProjectToolStripMenuItem
+            // 
+            saveProjectToolStripMenuItem.Name = "saveProjectToolStripMenuItem";
+            saveProjectToolStripMenuItem.Size = new Size(146, 22);
+            saveProjectToolStripMenuItem.Text = "Save project";
+            // 
+            // importMediaToolStripMenuItem
+            // 
+            importMediaToolStripMenuItem.Name = "importMediaToolStripMenuItem";
+            importMediaToolStripMenuItem.Size = new Size(146, 22);
+            importMediaToolStripMenuItem.Text = "Import media";
+            importMediaToolStripMenuItem.Click += importMediaToolStripMenuItem_Click;
+            // 
+            // importFolderToolStripMenuItem
+            // 
+            importFolderToolStripMenuItem.Name = "importFolderToolStripMenuItem";
+            importFolderToolStripMenuItem.Size = new Size(146, 22);
+            importFolderToolStripMenuItem.Text = "Import folder";
+            // 
+            // viewToolStripMenuItem
+            // 
+            viewToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { secondDisplayWidowtempToolStripMenuItem });
+            viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            viewToolStripMenuItem.Size = new Size(44, 20);
+            viewToolStripMenuItem.Text = "View";
+            // 
+            // secondDisplayWidowtempToolStripMenuItem
+            // 
+            secondDisplayWidowtempToolStripMenuItem.Name = "secondDisplayWidowtempToolStripMenuItem";
+            secondDisplayWidowtempToolStripMenuItem.Size = new Size(227, 22);
+            secondDisplayWidowtempToolStripMenuItem.Text = "Second display widow(temp)";
+            // 
+            // AddMediaDialog
+            // 
+            AddMediaDialog.FileName = "AddMediaDialog";
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(47, 63);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(247, 489);
+            listView1.TabIndex = 4;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageSize = new Size(32, 32);
+            imageList1.TransparentColor = Color.Transparent;
+            // 
+            // FileFromListContextMenu
+            // 
+            FileFromListContextMenu.Items.AddRange(new ToolStripItem[] { moveUpToolStripMenuItem, moveDownToolStripMenuItem, removeFromPlaylistToolStripMenuItem });
+            FileFromListContextMenu.Name = "contextMenuStrip1";
+            FileFromListContextMenu.Size = new Size(184, 70);
+            // 
+            // moveUpToolStripMenuItem
+            // 
+            moveUpToolStripMenuItem.Name = "moveUpToolStripMenuItem";
+            moveUpToolStripMenuItem.Size = new Size(183, 22);
+            moveUpToolStripMenuItem.Text = "move up";
+            // 
+            // moveDownToolStripMenuItem
+            // 
+            moveDownToolStripMenuItem.Name = "moveDownToolStripMenuItem";
+            moveDownToolStripMenuItem.Size = new Size(183, 22);
+            moveDownToolStripMenuItem.Text = "move down";
+            // 
+            // removeFromPlaylistToolStripMenuItem
+            // 
+            removeFromPlaylistToolStripMenuItem.Name = "removeFromPlaylistToolStripMenuItem";
+            removeFromPlaylistToolStripMenuItem.Size = new Size(183, 22);
+            removeFromPlaylistToolStripMenuItem.Text = "remove from playlist";
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1119, 635);
+            Controls.Add(listView1);
+            Controls.Add(toolStrip1);
+            Controls.Add(menuStrip1);
+            Controls.Add(PreviewBox);
+            MainMenuStrip = menuStrip1;
+            Name = "MainWindow";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)PreviewBox).EndInit();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            FileFromListContextMenu.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox PreviewBox;
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButton1;
+        private ToolStripButton toolStripButton2;
+        private ToolStripButton toolStripButton3;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem newProjectToolStripMenuItem;
+        private ToolStripMenuItem openProjectToolStripMenuItem;
+        private ToolStripMenuItem saveProjectToolStripMenuItem;
+        private ToolStripMenuItem importMediaToolStripMenuItem;
+        private ToolStripMenuItem importFolderToolStripMenuItem;
+        private ToolStripMenuItem viewToolStripMenuItem;
+        private ToolStripMenuItem secondDisplayWidowtempToolStripMenuItem;
+        private OpenFileDialog AddMediaDialog;
+        private ListView listView1;
+        private ImageList imageList1;
+        private ContextMenuStrip FileFromListContextMenu;
+        private ToolStripMenuItem moveUpToolStripMenuItem;
+        private ToolStripMenuItem moveDownToolStripMenuItem;
+        private ToolStripMenuItem removeFromPlaylistToolStripMenuItem;
     }
 }
--- second-display-media-control/Form1.cs
+++ second-display-media-control/Form1.cs
@@ -1,10 +1,42 @@
+using System;
+using System.IO;
+using System.Windows.Forms;
+
 namespace second_display_media_control
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+            listView1.AllowDrop = true;
+            listView1.MultiSelect = true;
+            listView1.Columns.Add("Preview", 60);
+            listView1.Columns.Add("Name", 200);
+            listView1.Columns.Add("Path", 400);
+            listView1.SmallImageList = imageList1;//connects listview thing with list of actual images
+        }
+
+        private void importMediaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddMediaDialog.Filter = "Media files|*.mp4;*.avi;*.mov;*.mkv;*.jpg;*.jpeg;*.png;*.bmp";
+            AddMediaDialog.Multiselect = true;
+            if (AddMediaDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string filePath in AddMediaDialog.FileNames)
+                {
+                    Icon fileIcon = Icon.ExtractAssociatedIcon(filePath);//temporary thing, will change to actual thumbnail after conneccting ffmpeg/mpv
+                    imageList1.Images.Add(filePath, fileIcon);
+                    var item = new ListViewItem("", imageList1.Images.Count - 1);
+                    item.SubItems.Add(Path.GetFileName(filePath));
+                    item.SubItems.Add(filePath);
+                    item.Tag = filePath;
+                    listView1.Items.Add(item);
+                }
+            }
         }
     }
 }
+    
